75b275047868951a722ccccc96c8b33d36175475
Replit user <replituser@example.com>
2021-03-27 00:47:56 +0000

    new try

--- list.c
+++ list.c
@@ -123,9 +123,9 @@ void * popCurrent(List * list) {
   
   if(list->current == list->head){
   
-  list->head = list->current->next;
+  list->head = list->current->next->prev;
   
-  list->head->next->prev = NULL;
+  list->head->prev = NULL;
    
   }
   if(list->current->prev != NULL && list->current->next !=NULL){
